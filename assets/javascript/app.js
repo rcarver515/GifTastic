827d19bf4b4a9c5ef06a47f635b6857113a16edd
Robert Grant <rcarver515@gmail.com>
2019-03-05 16:50:11 -0600

    code update: bootstrap buttons added

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,20 +1,52 @@
-$(document).ready(function() {
+$(document).ready(function () {
 
-// Create an array and save it to a variable called topics
-var heroTopics = [
-    "Blue Marvel","Black Lightning","Black Panther","Miles Morales"
-]
+    // Create an array and save it to a variable called topics
+    var heroTopics = ["Batman", "Superman", "The Flash", "Aquaman", "Cyborg", "Wonder Woman"];
 
-for (var i = 0; i , heroTopics.length; i++) {
+    $("btn btn-primary").on("click", function () {
+
+        var hero = $(this).attr("data-name");
+
+        var queryURL = "https://api.giphy.com/v1/gifs/search?q=batman";
+
+        hero + "&api_key=wHCSuOfsZ0RmYMldeKM8aVMiOd5vUtFU=5"
+
+        // Ajax GET request
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        })
+
+            .then(function (response) {
+
+                // Create a div(button) to hold hero name
+
+                var heroName = response.data;
+
+                for (var i = 0; i < heroTopics.length; i++) {
+                    console.log(heroTopics[1])
+
+                    var heroDiv = $("<div class='hero'>");
 
 
-}
+                    var p = $("<p>").text("Name: " + heroName);
+
+                    var heroImage = $("<img>");
+
+                    heroImage.attr("src", results[i].images.fixed - height.url)
+
+                    heroDiv.append(p);
+                    heroDiv.append(heroImage)
+
+                    var imgURL = response.data.image_original_url;
 
 
+                    heroImage.attr("src", imgURL);
+                    heroImage.attr("alt", "hero image");
 
+                    $("#gifs-appear-here").prepend(heroDiv);
 
-
-
-
-
-}
+                }
+            })
+            })
+            })
