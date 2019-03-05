689cf767cae991b15cdfe8de203ba7b1fe7f003b
Robert Grant <rcarver515@gmail.com>
2019-03-05 10:54:21 -0600

    folders and files

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,5 +1,14 @@
 $(document).ready(function() {
 
+// Create an array and save it to a variable called topics
+var heroTopics = [
+    "Blue Marvel","Black Lightning","Black Panther","Miles Morales"
+]
+
+for (var i = 0; i , heroTopics.length; i++) {
+
+
+}
 
 
 
@@ -8,5 +17,4 @@ $(document).ready(function() {
 
 
 
-    
 }
